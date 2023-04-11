90fa41429d8e3e76f51f9c449f6ef302c675da0a
nurrative <nursul1997@gmail.com>
2023-04-11 12:02:35 +0600

    post listing with templates

--- blog/config/urls.py
+++ blog/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from post.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('listing/',post_list)
 ]
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    queryset = Post.objects.all()
+
+    return render(request, 'listing.html', {"posts":queryset})
+
