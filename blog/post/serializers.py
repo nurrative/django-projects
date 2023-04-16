fbd580a12e1b123bd5cab5393b166385eed0fde1
nurrative <nursul1997@gmail.com>
2023-04-16 18:22:02 +0600

    test

--- blog/post/serializers.py
+++ blog/post/serializers.py
@@ -1,32 +1,3 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Post, Comment
-
-# class PostSerializer(ModelSerializer):
-#     class Meta:
-#         model = Post
-#         fields = '__all__'
-
-#     def to_representation(self, instance):
-#         rep = super().to_representation(instance)
-#         rep['likes'] = instance.likes.all().count()
-#         rep['comments'] = instance.comments.all()
-#         return rep
-class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
-
-    class Meta:
-        model = Comment
-        fields = ['id', 'user', 'content', 'created_at']
-
-class PostSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        rep['likes'] = instance.likes.all().count()
-        return rep
